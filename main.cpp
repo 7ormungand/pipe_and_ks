dc9cc157fa00f7fe4cb2b4a69850a0f5e47dc1c4
Alexey Goretsky <alex.chi.07@mail.ru>
2026-09-11 19:01:46 +0300

    добавлены struct PIpe и Ks

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+
+
+
+
+struct Pipe {
+    std::string name;
+    int length;
+    int d;
+    bool work;
+};
+
+struct Ks {
+    std::string name;
+    int c_shop;
+    int c_shop_w;
+    bool work;
+    char cl;
+};
+
+int main() {
+    setlocale(LC_ALL, "ru-RU.UTF-8");
+    Pipe p = {};
+    return 0;
+}
